a3d42ec1d607e49454f90fbb0dbbb41a8258b31a
xZezaru <xzezaru@gmail.com>
2024-04-18 01:22:27 +0200

    dialApp inicio

---
+++ dial.js
@@ -0,0 +1,28 @@
+console.log("Corriendo dial");
+
+//  Obtener el contexto de dibujo
+var canvas = document.getElementById("canvas"); // Suponiendo que el ID de tu canvas es "miCanvas"
+var ctx = canvas.getContext("2d");
+
+// Establecer el estilo de dibujo
+ctx.fillStyle = "#202020"; // Color negro
+
+const punto = (x, y) => {
+  ctx.beginPath();
+  ctx.arc(x, y, 6, 0, Math.PI * 2);
+  ctx.fill();
+  // Eliminar el punto después de 5 segundos
+  setTimeout(() => borrarPunto(x, y), 1000);
+}
+
+const borrarPunto = (x, y) => {
+  ctx.clearRect(x - 6, y - 6, 12, 12); // Borrar un área cuadrada del tamaño del punto
+}
+
+// Listener
+window.addEventListener('click', function (evento) {
+  var x = evento.clientX;
+  var y = evento.clientY;
+
+  punto(x, y);
+});
